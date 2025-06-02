aafda9dc873dbc74fd799296d888af9d0867c46f
Maryamfati <maryamfati5123@gmail.com>
2025-06-02 15:04:04 +0500

    Analaytics

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {BlogsProvider} from "@/Context/blogcontext"
+import { BlogsProvider } from "@/Context/blogcontext"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,15 +23,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B91X37J8R8"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-B91X37J8R8');
+  `}
+        </Script>
+
+      </head>
 
 
       <body
         className=' bg-whiteoff  !overflow-x-hidden'
       >
+        <Analytics />
         <Navbar />
         <BlogsProvider>
 
-        {children}
+          {children}
         </BlogsProvider>
         <Footer />
 
